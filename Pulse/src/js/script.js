db09b284b1e3d5cade5f76bb4709955b417e6ba9
Vladyslav Merezhko <merezhko.vlad@gmail.com>
2022-11-18 21:16:08 +0200

    add catalog

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -1,9 +1,10 @@
+// tiny-slider
 const slider = tns({
 	container: '.carousel__inner',
 	items: 1,
 	slideBy: 'page',
 	controls: false,
-	autoplay: true,
+	// autoplay: true,
 	nav: true,
 	navPosition: 'bottom',
 	autoplayButtonOutput: false,
@@ -35,3 +36,50 @@ function autoHeight() {
 	sliderInner.style.height = newH + 'px';
 }
 autoHeight();
+
+// Tabs
+const catalogTab = document.querySelectorAll('.catalog__tab'),
+	catalogContent = document.querySelectorAll('.catalog__content');
+
+catalogTab.forEach((item, i) => {
+	item.addEventListener('click', () => {
+		catalogTab.forEach(item => {
+			item.classList.remove('catalog__tab_active');
+		});
+		item.classList.add('catalog__tab_active');
+		catalogContent.forEach(item => {
+			item.classList.remove('catalog__content_active');
+		});
+		catalogContent[i].classList.add('catalog__content_active');
+	});
+});
+
+// item links
+const catalogItemLinks = document.querySelectorAll('.catalog-item__link'),
+	catalogItemBack = document.querySelectorAll('.catalog-item__back'),
+	catalogItemContent = document.querySelectorAll('.catalog-item__content'),
+	catalogItemList = document.querySelectorAll('.catalog-item__list');
+
+// toggle slide
+function toggleSlide(item) {
+	item.forEach((item, i) => {
+		item.addEventListener('click', (e) => {
+			e.preventDefault();
+			catalogItemContent[i].classList.toggle('catalog-item__content_active');
+			catalogItemList[i].classList.toggle('catalog-item__list_active');
+		});
+	});
+}
+toggleSlide(catalogItemLinks);
+toggleSlide(catalogItemBack);
+
+function checkTouch() {
+	if (('ontouchstart' in window) || window.DocumentTouch && document instanceof window.DocumentTouch) {
+		document.body.classList.add('touch');
+		console.log('this is a touch device');
+	} else {
+		console.log('this is not a touch device');
+		document.body.classList.add('no_touch');
+	}
+}
+checkTouch();
